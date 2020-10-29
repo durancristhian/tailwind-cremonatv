8d11ad2f72501dd0b6850550691984ef2badc46b
Cristhian Duran <durancristhian@gmail.com>
2020-10-29 20:26:09 -0300

    Episodio 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Index() {
-  return <h1>tailwind-cremonatv</h1>
+  return <h1 className="text-center text-blue-700">tailwind-cremonatv</h1>
 }
---
+++ tailwind.config.minimal.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    /* Permite extender algo del tema sin modificar lo que viene por defecto */
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
